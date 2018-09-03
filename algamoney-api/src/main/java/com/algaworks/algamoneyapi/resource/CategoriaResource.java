06a557bd6fad1eaa00d6ef2bb07798529739de29
Mariana Loureiro <mloureiro@plusoft.com.br>
2018-09-03 17:21:24 -0300

    alteracoes em CategoriaResource

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -1,14 +1,20 @@
 package com.algaworks.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
@@ -21,13 +27,30 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> listar() {
-		return categoriaRepository.findAll(); 
+	public ResponseEntity<List<Categoria>> listar() {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		if (!categorias.isEmpty()) {
+			return ResponseEntity.ok(categorias);
+		}
+		return ResponseEntity.noContent().build();
 	}
 	
-	@GetMapping("/todos")
-	public HttpEntity<List<Categoria>> listarTodos() {
-		return new ResponseEntity<List<Categoria>>(categoriaRepository.findAll(), HttpStatus.OK);
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria novaCategoria = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(novaCategoria.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(novaCategoria);
 	}
 	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPorCodigo(@PathVariable Long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		if (categoria != null) {
+			return ResponseEntity.ok(categoria);
+		}
+		return ResponseEntity.notFound().build();
+	}
 }
