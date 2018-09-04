903ccc9f2b18ce86613fba10ffb5195dadcc8832
Mariana Loureiro <mloureiro@plusoft.com.br>
2018-09-04 16:01:30 -0300

    Classe exception handler

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyApiExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.algaworks.algamoneyapi.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AlgamoneyApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		// TODO Auto-generated method stub
+		return super.handleHttpMessageNotReadable(ex, headers, status, request);
+	}
+	
+}
