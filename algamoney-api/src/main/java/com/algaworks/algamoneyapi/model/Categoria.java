a65bcd9778ea0cc8abf65481bdb5a42789131ca4
Mariana Loureiro <mloureiro@plusoft.com.br>
2018-09-10 11:49:14 -0300

    removendo @table de classe categoria e testando configuracoes no properties

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Categoria.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Categoria.java
@@ -4,10 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "categoria")
+//@Table(name = "categoria")
 public class Categoria {
 	
 	@Id
